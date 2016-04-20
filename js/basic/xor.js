1d29149d0a5f9d6759ad33a3721c9c0f8c89eb3b
zacanger <zac@zacanger.com>
2016-04-20 16:27:28 -0600

    :arrow_up: xor

--- js/basic/xor.js
+++ js/basic/xor.js
@@ -2,7 +2,17 @@
 // xor(false, false) // => false
 // xor(true, false)  // => true
 
+const xor = (a, b) => !a != !b
+
 function xor(x, y){
   return(!x != !y)
+}
+
+let xor = (val1, val2) => {
+  if (val1 && val2 || !val1 && val2) {
+    return false
+  } else {
+    return true
+  }
 }
 
