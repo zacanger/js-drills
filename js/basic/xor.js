a0330bd0d18adc2168b814479ae4212e726b28dc
zacanger <zac@zacanger.com>
2016-04-20 16:31:44 -0600

    :bar_chart: xor

--- js/basic/xor.js
+++ js/basic/xor.js
@@ -2,17 +2,33 @@
 // xor(false, false) // => false
 // xor(true, false)  // => true
 
+// perf.
 const xor = (a, b) => !a != !b
 
+// the above, just in more words
 function xor(x, y){
   return(!x != !y)
 }
 
+// verbosely
 let xor = (val1, val2) => {
   if (val1 && val2 || !val1 && val2) {
     return false
   } else {
     return true
   }
+}
+
+// ternary operator?
+var xor = function(thingOne, thingTwo){
+  if(thingOne ? !thingTwo : thingTwo) {
+    return true
+  }
+  return false
+}
+
+// using the bitwise XOR
+function XOR (x, y) {
+  return (x ? 1 : 0) ^ (y ? 1 : 0)
 }
 
