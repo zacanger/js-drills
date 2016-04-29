b7e11108e22537435e5df247813a74913156b19a
zacanger <zac@zacanger.com>
2016-04-29 16:22:38 -0600

    foo

---
+++ node-drills/node/10/index.js
@@ -0,0 +1,3 @@
+// write a rock-paper-scissors game in node!
+// we'll want to run it like `./rock-paper-scissors.js rock`
+// (or paper, or scissors, obviously).
---
+++ node-drills/node/11/index.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+// write an interactive tic-tac-toe game that runs in the terminal!
