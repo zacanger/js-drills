a5b87563ddde0117ff9aa6cedacc28dae5fd99b9
zacanger <zac@zacanger.com>
2016-04-29 15:17:30 -0600

    foo

--- js/basic/triangle.js
+++ js/basic/triangle.js
@@ -1,7 +1,15 @@
 // log out a triangle of hashes (#)
 
-var txt = '#######'
-for(var i = txt.length; i >= 0; i--){
+const txt = '#######'
+for (let i = txt.length; i >= 0; i--) {
   console.log(txt.slice(i))
+}
+
+// or, taking an argument for size
+const num = process.argv[2]
+let str   = ''
+for (let i = num; i >= 1; i--) {
+  str += '#'
+  console.log(str)
 }
 
