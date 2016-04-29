8353b902b8a515b691123e8917f1f47db0144e3d
zacanger <zac@zacanger.com>
2016-04-29 15:07:35 -0600

    foo

--- js/basic/chessboard.js
+++ js/basic/chessboard.js
@@ -1,15 +1,18 @@
 // make a chess board!
 
-var size = 5
-  , cb   = ''
+var
+  size = 5
+, cb   = ''
 
 for (var i = 1; i <= size; i++) {
   var charp = false
+
   if (i % 2 === 0) {
     charp = true
   } else {
     charp = false
   }
+
   for (var j = 1; j <= size; j++) {
     if (charp) {
       if (j % 2 === 0) {
@@ -17,6 +20,7 @@ for (var i = 1; i <= size; i++) {
       } else {
         cb += '#'
       }
+
     } else {
       if (j % 2 === 0) {
         cb += '#'
