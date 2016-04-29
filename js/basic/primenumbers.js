274c87c2bc35dd924facb5862232c4844c9a3d42
zacanger <zac@zacanger.com>
2016-04-29 14:17:13 -0600

    foo

--- js/basic/primenumbers.js
+++ js/basic/primenumbers.js
@@ -1,8 +1,8 @@
-// Write a function that accepts a number and returns a boolean based on whether it's a prime number.
+// write a function that accepts a number and returns a boolean based on whether it's a prime number
 
 function primeFinder(num){
-  for(var i = 2; i < num; i++){
-    if(num % i === 0 && i !== num){
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0 && i !== num) {
       return false
     }
   }
@@ -13,8 +13,8 @@ function primeFinder(num) {
   if (num % 2  === 0) {
     return false
   }
-  var middle = ~~Math.sqrt(num)
-  for (var i = 3; i <= middle; i += 2) {
+  let middle = ~~Math.sqrt(num)
+  for (let i = 3; i <= middle; i += 2) {
     if (num % i === 0) {
       return false
     }
@@ -22,16 +22,15 @@ function primeFinder(num) {
   return true
 }
 
-
 function primeFinder(num){
-  if(num === 2){
+  if (num === 2) {
     return true
   }
-  if(num < 2 || !(num&1)){
+  if (num < 2 || !(num&1)) {
     return false
   }
-  for(var i = 3, l = Math.floor(Math.pow(num, 0.5)); i <= l; i+=2){
-    if(num % i === 0){
+  for (let i = 3, l = Math.floor(Math.pow(num, 0.5)); i <= l; i+=2) {
+    if (num % i === 0) {
       return false
     }
   }
