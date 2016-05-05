4572e4b5d78dbf55f62d28e850305b5e94787a83
zacanger <zac@zacanger.com>
2016-05-05 17:46:36 -0600

    :rage3: stockticker

--- js/advanced/stock-ticker.js
+++ js/advanced/stock-ticker.js
@@ -1,6 +1,7 @@
-//I have an array of 24 stock prices for each hour tomorrow,
-// e.g. [20, 15, 1, 10, 15, 8, 16, 17, 8, 13, 10, 11, 19, 7, 6, 12, 6, 6, 6, 2, 11, 5, 1, 5].
-  //I start with no stock and can only buy and sell once, otherwise the time police will get me.
-    // Write a function that lets me know which hour to buy and which hour to sell to maximize my profit.
+// I have an array of 24 stock prices for each out tomorrow, e.g.
+// [20, 15, 1, 10, 15, 8, 16, 17, 8, 13, 10, 11, 19, 7, 6, 12, 6, 6, 6, 2, 11, 5, 1, 5]
+// I start with no stock and can only buy and sell once, otherwise the time police will get me.
+// Write a function that lets me know which hour to buy and which hour to sell to maximize my profit.
 
-        //For the above array, your function should return [2,12]
+// For the above array, your function should return [2, 12].
+
