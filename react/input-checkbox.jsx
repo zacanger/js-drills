3fe4321e46e8747875402f3595021a1f1291eafe
zacanger <zac@zacanger.com>
2016-05-07 19:26:48 -0600

    :cn: React !

---
+++ react/input-checkbox.jsx
@@ -0,0 +1,5 @@
+// make an app that has a text input, checkbox, and header
+// the header's text should be whatever is in the input
+// its colour should be red if the checkbox is checked, and black otherwise
+// render to a div with an id of 'root'
+
---
+++ react/simplest-component.jsx
@@ -0,0 +1,3 @@
+// Write a component that renders an H1 that says something!
+// Use ReactDOM.render to target a div on your (imaginary) HTML
+
