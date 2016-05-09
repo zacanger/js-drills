cdc44121a7ce14f39d4616ba57264d26118b25ec
zacanger <zac@zacanger.com>
2016-05-09 14:03:36 -0600

    :page_facing_up: iterable

---
+++ js/intermediate/iterable.js
@@ -0,0 +1,15 @@
+// create an object called iterable
+// iterable has two methods
+//
+// @[Symbol.iterator]
+// returns an object with a next method
+// takes array given from set method
+// and returns the square of them when
+// the next method is called
+
+// @set
+// takes an arbirary amount of numbers (not an array)
+// stores them for use with the iterator
+
+// code goes here
+
