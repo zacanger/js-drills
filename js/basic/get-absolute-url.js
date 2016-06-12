8abe6965649e83b2860510469aa1d41b173b44dc
zacanger <zac@zacanger.com>
2016-06-12 14:34:08 -0600

    abs

---
+++ js/basic/get-absolute-url.js
@@ -0,0 +1,14 @@
+// get an absolute url from a string variable. assuming you're on the example.com domain:
+// getAbsoluteUrl('/foo') // => http://example.com/foo
+
+var getAbsoluteUrl = (function() {
+	var a
+	return function(url) {
+    if (!a) {
+      a = document.createElement('a')
+    }
+		a.href = url
+		return a.href
+	}
+})()
+
