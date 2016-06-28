f980d7675492da9056b73f1ca0ec1eea818a5604
zacanger <zac@zacanger.com>
2016-06-28 10:11:24 -0600

    thenable

---
+++ js/basic/thenable-checker.js
@@ -0,0 +1,8 @@
+// write a 'thenable checker' function that takes an argument
+// and finds out if it returns a promise
+
+const thenable = a => {
+  if (typeof a.then === 'function') {
+    return true
+  }
+}
