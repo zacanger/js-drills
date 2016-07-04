ab88e1d8b47125844ad640b6ed95c53530d755a5
zacanger <zac@zacanger.com>
2016-07-04 09:46:41 -0500

    maxprofit

---
+++ js/intermediate/max-profit.js
@@ -0,0 +1,6 @@
+// Write a function that takes in an array of stock prices, as numbers, and
+// determine what the maximum possible profit from that day. Remember that you
+// cannot sell before you buy. The function should return an object containing the
+// maximum profit, and the buy and sell indices
+
+// usage : maxProfit([10, 7, 5, 8, 11, 9]) // {buyIndex : 2, sellIndex : 4, profit : 6}
