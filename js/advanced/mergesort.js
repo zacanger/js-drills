33f5adfca8d9278fdec20852197f164ca09b1ba9
zacanger <zac@zacanger.com>
2016-07-04 21:06:32 -0600

    mergesort

--- js/advanced/mergesort.js
+++ js/advanced/mergesort.js
@@ -55,3 +55,53 @@ var merge = function (array, lo, mid, hi) {
     array[current++] = helper[i]
   }
 }
+
+
+//
+//
+var mergeSort = function (array) {
+  var lists = []
+  // Split array into sublists
+  // Natural variant: split array into pre-sorted sublists
+  var currentList = []
+  for (var i = 0; i < array.length; i++) {
+    if (currentList.length && array[i] < currentList[currentList.length - 1]) {
+      lists.push(currentList)
+      currentList = []
+    }
+    currentList.push(array[i])
+  }
+  lists.push(currentList)
+  // Until the entire array is sorted
+  while (lists.length > 1) {
+    var newLists = []
+    // Merge all adjacent lists
+    for (var i = 0; i < Math.floor(lists.length / 2); i++) {
+      newLists.push(merge(lists[i * 2], lists[i * 2 + 1]))
+    }
+    // Include the leftover list if the number is odd
+    if (lists.length % 2) {
+      newLists.push(lists[lists.length - 1])
+    }
+    lists = newLists
+  }
+  // We have a single, fully sorted list
+  return lists[0]
+}
+
+var merge = function (left, right) {
+  var merged = []
+  var iL = 0
+  var iR = 0
+  while (merged.length < left.length + right.length) {
+    // Default to the left element for stability
+    if (iR >= right.length || left[iL] <= right[iR]) {
+      merged.push(left[iL])
+      iL += 1
+    } else {
+      merged.push(right[iR])
+      iR += 1
+    }
+  }
+  return merged
+}
