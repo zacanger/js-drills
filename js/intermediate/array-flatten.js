2915e0af8053bc1422dcf0e2b3d0ff109d7b730a
zacanger <zac@zacanger.com>
2016-07-05 20:34:58 -0600

    flatten-array.js

--- js/intermediate/array-flatten.js
+++ js/intermediate/array-flatten.js
@@ -82,3 +82,37 @@ function flatten(array) {
 
 //
 const flattened = arg.reduce((a, b) => a.concat(b))
+
+//
+function flatten (arr) {
+  var flatArr = [], i
+  function nestedCheck (arr1) {
+    for (var j = 0;j < arr1.length;j++) {
+      if (!Array.isArray(arr1[j]))
+        flatArr.push(arr1[j])
+      else nestedCheck(arr1[j])
+    }
+  }
+  for (i = 0;i < arr.length;i++) {
+    if (!Array.isArray(arr[i]))
+      flatArr.push(arr[i])
+    else nestedCheck(arr[i])
+  }
+  return flatArr
+}
+
+//
+function flatten (array) {
+  var flatArray = []
+  for (var i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      var newArray = flatten(array[i])
+      for (var j = 0; j < newArray.length; j++) {
+        flatArray.push(newArray[j])
+      }
+    } else {
+      flatArray.push(array[i])
+    }
+  }
+  return flatArray
+}
