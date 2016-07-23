e507c0b77b16a56c66f7b653cb664d0e6b178b38
zacanger <zac@zacanger.com>
2016-07-22 18:37:52 -0600

    note that you can use functions or classes

--- react/simple-app/src/Button.js
+++ react/simple-app/src/Button.js
@@ -1,3 +1,3 @@
 import React, {PropTypes} from 'react'
 
-// your code here!
+// your code here! remember, you can use a function or a class!
--- react/simple-app/src/Input.js
+++ react/simple-app/src/Input.js
@@ -1,5 +1,7 @@
 import React, {PropTypes} from 'react'
 
-// your code here!
+// your code here! either a function or a class will work. starter for a function is below.
+
+const Input = props => // ??
 
 Input.propTypes = { changename : PropTypes.func }
