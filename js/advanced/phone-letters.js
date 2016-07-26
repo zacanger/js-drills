36a9effd9d2efdd8fdf7234b24416156b753a253
zacanger <zac@zacanger.com>
2016-07-25 18:51:27 -0600

    advanced/phone-letters.js

--- js/advanced/phone-letters.js
+++ js/advanced/phone-letters.js
@@ -1,41 +1,42 @@
 #!/usr/bin/env node
 
-var input = process.argv[2]
+const input = process.argv[2]
 
-(function(){
-  var NUMBER_TO_LETTER_MAP = [
-    [],
-    [],
-    ['a', 'b', 'c'],
-    ['d', 'e', 'f'],
-    ['g', 'h', 'i'],
-    ['j', 'k', 'l'],
-    ['m', 'n', 'o'],
-    ['p', 'q', 'r', 's'],
-    ['t', 'u', 'v'],
-    ['w', 'x', 'y', 'z']
+const convert = () => {
+  const NUMBER_TO_LETTER_MAP = [
+    []
+  , []
+  , ['a', 'b', 'c']
+  , ['d', 'e', 'f']
+  , ['g', 'h', 'i']
+  , ['j', 'k', 'l']
+  , ['m', 'n', 'o']
+  , ['p', 'q', 'r', 's']
+  , ['t', 'u', 'v']
+  , ['w', 'x', 'y', 'z']
   ]
-  (function(input){
-    generateCombinations(input).forEach(function(s){
+  (input => {
+    generateCombinations(input).forEach(s => {
       console.log(s)
     })
   }(input))
   function generateCombinations(number){
-    var finalCombinations = []
+    const finalCombinations = []
     combineWords(finalCombinations, '', number)
     return finalCombinations
   }
   function combineWords(finalCombinations, prefix, remaining){
-    var letters = NUMBER_TO_LETTER_MAP[remaining.substr(0, 1)]
+    const letters = NUMBER_TO_LETTER_MAP[remaining.substr(0, 1)]
     if(remaining.length === 1){
-      letters.forEach(function (letter){
+      letters.forEach(letter => {
         finalCombinations.push(prefix + letter)
       })
     } else {
-      letters.forEach(function(letter){
+      letters.forEach(letter => {
         combineWords(finalCombinations, prefix + letter, remaining.substr(1))
       })
     }
   }
-}())
+}
 
+convert()
