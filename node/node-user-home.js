fc5464097dd7cbaf6655c4cefd6e8634ef451fc5
zacanger <zac@zacanger.com>
2016-07-25 19:36:13 -0600

    node-user-home.js

---
+++ node/node-user-home.js
@@ -0,0 +1,10 @@
+// write a function that checks to see what ~/
+// is called on a given machine. hint: it's not the same
+// on windows and not-windows.
+
+// either of these will work
+
+const home  = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME']
+const home2 = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE
+
+console.log(home, home2)
