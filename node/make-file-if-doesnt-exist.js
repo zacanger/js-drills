46cc0da8d94e443605f7e56a2542091b011c29a4
zacanger <zac@zacanger.com>
2016-07-25 19:39:35 -0600

    .

---
+++ node/make-file-if-doesnt-exist.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+// write a little cli tool to create a file
+// if it doesn't already exist.
+// assume it'll be a js file, and if it doesn't
+// already exist you want it to just contain 'use strict'
