d8eb6708a169e9828fa924056885b4396a5e57d8
zacanger <zac@zacanger.com>
2016-07-25 19:00:24 -0600

    14

---
+++ node/14/index.js
@@ -0,0 +1,11 @@
+// write a function that checks to see if a module is locally installed
+
+const isInstalled = pkgName => {
+  try {
+    require.resolve(pkgName)
+    return true
+  } catch (e) {
+    return false
+  }
+}
+export default isInstalled
