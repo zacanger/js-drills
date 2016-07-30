6ca0bdced00c48d7643269438805436f667fc192
zacanger <zac@zacanger.com>
2016-07-30 13:01:29 -0600

    check-up.js

---
+++ node/check-up.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+// write a small cli app you can call to check
+// if a website is up.
+// usage: check-up.js google.com
+
+const http = require('http')
+const host = process.argv[2]
+const opts = {
+  host
+, port : 80
+, path : '/'
+}
+
+http.get(opts, res => {
+  if (res.statusCode === 200) {
+    console.log(`${host} is up`)
+  } else {
+    console.log(`${host} is down`)
+  }
+}).on('error', err => {
+  console.warn(`Error: ${err.message}`)
+})
