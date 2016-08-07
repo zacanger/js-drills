7eb184df563ce0215489ad90e07d2a6aa2743edb
zacanger <zac@zacanger.com>
2016-08-06 19:13:59 -0600

    minimum-stack.js

---
+++ js/intermediate/minimum-stack.js
@@ -0,0 +1,8 @@
+// Implement a stack which keeps track of its minimum value.
+// Challenge: can you keep the operation time complexity to `O(1)`?
+// Hint: Try using some extra space. What data structure might work for keeping track of minimums?
+
+// your code here
+
+stack.pushAll([1, 2, 3, 5, 0])
+console.log(stack.getMinimum())
