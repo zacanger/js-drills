774ef725fafa9ceaa2fba513ac9339cdac8dfa12
zacanger <zac@zacanger.com>
2016-08-24 12:48:57 -0600

    map-parseInt.js

---
+++ js/basic/map-parseInt.js
@@ -0,0 +1,10 @@
+// mapping parseInt over an array will lead to some funky results.
+const a = ['10', '10', '10']
+a.map(parseInt) // [10, NaN, 2]
+// fix this so it does what you'd expect!
+
+a.map(n => parseInt(n, 10))
+// or in old js:
+a.map(function(n) {
+  return parseInt(n, 10)
+})
