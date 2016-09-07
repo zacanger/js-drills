c238c159eabcce94e59100fdee22e8da3de9fec9
zacanger <zac@zacanger.com>
2016-09-06 21:29:40 -0600

    prompt-repeater.js

---
+++ node/prompt-repeater.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+// write a small node script
+// when you run it, it should say 'received data:' followed by anything you type in
+// if you type 'quit', it should say 'bye', and then exit
+
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+
+const { inspect } = require('util')
+
+const done = () => {
+  console.log('bye')
+  process.exit()
+}
+
+process.stdin.on('data', text => {
+  console.log('received data:', inspect(text))
+  if (text === 'quit\n') {
+    done()
+  }
+})
