966c45e910c7952c32763682f4c43c0ce4cb794e
zacanger <zac@zacanger.com>
2016-09-06 21:30:01 -0600

    prompt-repeater.js

---
+++ node/prompt-repeater.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+// write a small node script
+// when you run it, it should say 'received data:' followed by anything you type in
+// if you type 'quit', it should say 'bye', and then exit
