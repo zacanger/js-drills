97bf2c46409795b452cd893dbced1063310beefb
zacanger <zac@zacanger.com>
2016-09-08 14:10:43 -0600

    index-of.js

---
+++ js/basic/index-of.js
@@ -0,0 +1,13 @@
+const indexOf = [].indexOf
+
+module.exports = function (arr, obj) {
+  if (indexOf) {
+    return arr.indexOf(obj)
+  }
+  for (let i = 0; i < arr.length; ++i) {
+    if (arr[i] === obj) {
+      return i
+    }
+  }
+  return -1
+}
