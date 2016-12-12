f3b256b9b1504e761d0df57162c845738a94fcb4
zacanger <zac@zacanger.com>
2016-12-11 17:24:44 -0700

    compose-pipe.js

---
+++ js/intermediate/compose-pipe.js
@@ -0,0 +1,24 @@
+// write compose and pipe functions
+// compose should return a function that is the composition
+// of a list of functions, right to left
+// pipe should return the a function that is the result
+// of a series of functions called on the result of the previous
+// function, left to right
+
+function compose () {
+  const args = Array.prototype.slice.call(arguments)
+  return function (val) {
+    return args.reduceRight(function(memo, func) {
+      return func(memo)
+    }, val)
+  }
+}
+
+function pipe () {
+  const args = Array.prototype.slice.call(arguments)
+  return function (val) {
+    return args.reduce(function(memo, func) {
+      return func(memo)
+    }, val)
+  }
+}
