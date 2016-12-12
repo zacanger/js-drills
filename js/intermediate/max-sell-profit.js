837e236495ce9f21d62a4b9de1b7ca874c64ca95
zacanger <zac@zacanger.com>
2016-12-11 17:14:28 -0700

    max-sell-profit.js

---
+++ js/intermediate/max-sell-profit.js
@@ -0,0 +1,4 @@
+// given a list of stock prices for n  days,
+// find the max profit with a single buy/sell activity
+// if you can't maximize buy/sell profit and can't make
+// a profit, minimize loss
