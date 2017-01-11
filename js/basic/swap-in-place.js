27f679e8985665e12cc6954c5c38d5c2feb75444
zacanger <zac@zacanger.com>
2017-01-10 20:11:49 -0700

    swap-in-place.js

---
+++ js/basic/swap-in-place.js
@@ -0,0 +1,19 @@
+// given two variables, swap them in place
+// this means swap their values, without creating a
+// third temp variable
+
+let a = 1
+let b = 2
+
+// your code here
+a = b - a
+b = b - a
+a = a + b
+
+// alternatively
+a = a + b
+b = a - b
+a = a - b
+
+// or with es2015
+[ b, a ] = [ a, b ]
