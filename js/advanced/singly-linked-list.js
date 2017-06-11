fb000af5e4dc2ea84818ea6cc0fbb305d15eeead
zacanger <zac@zacanger.com>
2017-06-11 12:49:45 -0600

    tree

--- js/advanced/singly-linked-list.js
+++ js/advanced/singly-linked-list.js
@@ -136,3 +136,55 @@ class LinkedList {
     this.length--
   }
 }
+
+//
+class List {
+  constructor () {
+    this.memory = []
+    this.length = 0
+  }
+
+  get (addr) {
+    return this.memory[addr]
+  }
+
+  push (val) {
+    this.memory[this.length] = val
+    this.length++
+  }
+
+  pop (val) {
+    if (this.length === 0) {
+      return
+    }
+    const last = this.length - 1
+    const val = this.memory[last]
+    delete this.memory[last]
+    this.length--
+    return val
+  }
+
+  unshift (val) {
+    let prev = val
+    for (let addr = 0; addr < this.length; addr++) {
+      let curr = this.memory[addr]
+      this.memory[addr] = prev
+      prev = curr
+    }
+    this.memory[this.length] = prev
+    this.length++
+  }
+
+  shift () {
+    if (this.length === 0) {
+      return
+    }
+    const val = this.memory[0]
+    for (let addr = 0; addr < this.length; addr++) {
+      this.memory[addr] = this.memory[addr + 1]
+    }
+    delete this.memory[this.length - 1]
+    this.length--
+    return val
+  }
+}
